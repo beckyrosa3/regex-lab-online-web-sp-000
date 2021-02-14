204b60e6b934a4682cafea32982e7b2ebff079be
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-14 01:49:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  /[a-z]{5}/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  /[0-9]{10}/ 
 end
