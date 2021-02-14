2b73fa4c569b38df76e2da528fbcc7c8fbeaa1f2
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-14 02:56:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  /\s[aeiou]/
+  word.scan(/\s[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  /[un][ing]/
+  text.scan(/[un][ing]/)
 end
 
 def words_five_letters_long(text)
